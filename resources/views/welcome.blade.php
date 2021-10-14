c523a59c35fe30dd07232a0de23d2d32975a52a2
William <work@suppo.fr>
2021-10-14 14:01:50 +0200

    Changement du titre.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,10 +24,10 @@
         <div class="min-h-screen bg-repeat" style="background-image: url('/images/pattern.png')">
             <div class="flex content-center flex-wrap min-h-screen">
                 <div class="w-2/3 md:w-1/3 mx-auto text-primary">
-                    <h1 class="text-8xl text-center font-semibold">Hi ! I'm <span class="text-secondary" style="text-shadow: 5px 5px #333333;">William</span></h1>
+                    <h1 class="text-8xl text-center font-semibold">Salut ! C'est <span class="text-secondary" style="text-shadow: 5px 5px #333333;">William</span></h1>
                     <img class="mt-12 rounded-full mx-auto shadow-lg" src="/images/avatar.jpg" alt="" width="300px">
                     <div class="mt-12 text-2xl text-justify">
-                        Salut, je suis Architecte logiciel.<br/>
+                        Je suis Architecte logiciel.<br/>
                         Père de 3 ptits monstres !<br/>
                         J'aime la tech et partager avec la communauté.<br/>
                         C'est pourquoi j'ai créé <a class="font-bold text-secondary hover:underline" href="https://neoshift.tv">Neoshift TV</a>.<br/>
