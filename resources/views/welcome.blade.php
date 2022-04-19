145a4e44d6474f5667757af5bb127d8b750b3580
William <work@suppo.fr>
2022-04-19 08:58:11 +0200

    Amélioration du design en md.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                 <i class="fab fa-linkedin-in"></i>
             </a>
         </div>
-        <div class="my-12 mx-auto md:w-1/3 px-4 md:px-0 flex flex-col justify-center md:px-0 pb-4 md:pb-0">
+        <div class="my-12 mx-auto md:w-1/2 px-4 md:px-0 flex flex-col justify-center md:px-0 pb-4 md:pb-0">
             <h3 class="text-3xl font-bold mb-4">Salut voyageur 👋</h3>
             <p class="mb-4 text-justify text-xl">
                 Je m'appelle William, je suis Lead développeur, expert PHP et Laravel.<br>
@@ -44,20 +44,22 @@
             </p>
             <p class="mb-4 text-justify text-xl">Je vous invite à me suivre sur <a class="text-primary hover:text-secondary underline" href="https://twitter.com/williamsuppo">Twitter</a> pour rester à l'affût de mes dernières news !</p>
         </div>
-        <div class="mx-auto md:w-1/3 px-4 md:px-0">
+        <div class="mx-auto md:w-1/2 px-4 md:px-0">
             <h3 class="text-3xl font-bold mb-4">Mes derniers articles 📰</h3>
             @foreach($posts as $post)
                 <article class="flex flex-col md:flex-row mb-4 pb-4 border-b border-gray-200">
-                    <img class="md:h-32 mb-4 md:mb-0 md:mr-4 rounded" src="{{ $post->image }}" alt="thumbnail"/>
-                    <div class="flex flex-col justify-between">
+                    <div class="mb-4 md:mb-0 md:mr-4">
+                        <img class="object-cover h-32 w-64 rounded mb-4" src="{{ $post->image }}" alt="thumbnail"/>
+                        <div class="text-gray-400 text-sm">
+                            <div>{{ $post->publishedAt->translatedFormat('j F Y') }}</div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-between w-2/3">
                         <a href="{{ $post->link }}" class="text-primary hover:text-secondary text-2xl font-bold mb-4">
                             {{ $post->title }}
                         </a>
                         <div class="mb-4">{{ $post->description }}</div>
-                        <div class="flex items-center text-gray-400 text-sm">
-                            <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                            <div>{{ $post->publishedAt->format('d/m/Y') }}</div>
-                        </div>
+                        <a href="{{ $post->link }}" class="text-primary hover:text-secondary">En savoir plus</a>
                     </div>
                 </article>
             @endforeach
