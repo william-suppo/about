cc73be8169e0b616242ad4f50aede1f36b6ddbd2
William <work@suppo.fr>
2022-04-24 15:51:19 +0200

    Ajout des analytics.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,23 @@
                 font-family: 'Roboto Condensed', sans-serif;
             }
         </style>
+
+        @env('production')
+            <!-- Analytics -->
+            <script>
+                var _paq = window._paq = window._paq || [];
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                    var u="//analytics.capslock.fun/";
+                    _paq.push(['setTrackerUrl', u+'matomo.php']);
+                    _paq.push(['setSiteId', '4']);
+                    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                })();
+            </script>
+        @endenv
     </head>
     <body class="antialiased">
         <header style="clip-path: ellipse(174% 100% at 53.72% 0%);" class="bg-electric-violet-700 py-14">
